66ca01eb77aa7592b21d25f5c9d3ec330057ccf6
Charles Clemens <cclemensjr@gmail.com>
2018-10-26 11:09:27 -0700

    Created Location object constructor

--- server.js
+++ server.js
@@ -5,10 +5,16 @@
 const express = require('express');
 const cors = require('cors');
 const superagent = require('superagent');
+const pg = require('pg');
 
 require('dotenv').config();
 
 const PORT = process.env.PORT;
+
+const client = new pageXOffset.Client(process.env.DATABASE_URL);
+client.connect();
+client.on('err', err => console.log(err));
+
 const app = express();
 
 app.use(cors());
@@ -18,7 +24,30 @@ app.use(cors());
  * GET REQUESTS
  */
 
- app.get('/location', getLocation);
- app.get('/weather', getWeather);
- app.get('/yelp', getYelp);
- app.get('/movies', getMovies);
+app.get('/location', getLocation);
+// app.get('/weather', getWeather);
+// app.get('/yelp', getYelp);
+// app.get('/movies', getMovies);
+
+
+/**
+ * Location
+ */
+
+function Location(query, data) {
+  this.search_query = query;
+  this.formatted_query = data.formatted_address;
+  this.latitude = data.geometry.location.lat;
+  this.longitude = data.geometry.location.lat;
+}
+
+Location.prototype.save = function {
+  let SQL = `INSERT INTO locations (search_query,formatted_query,latitude,longitude) VALUES($1,$2,$3,$4)`;
+  let values = Object.values(this);
+
+  clientInformation.query(SQL, values);
+}
+
+Location.fetchLocation = (query) => {
+
+}
